5a2db561419493f9f5e2d75323d04f1d67319249
opermaster <fran4er@gmail.com>
2024-04-06 11:58:16 +0300

    Added checking for bounds when FillRect, FillCircle

--- Example.cs
+++ Example.cs
@@ -7,18 +7,6 @@ using System.Threading.Tasks;
 
 namespace SavingPpm
 {
-    struct Point
-    {
-        public int x;
-        public int y;
-        public int z;
-
-        public Point(int x, int y, int z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-    }
     class Example {
   
         static void Main() {
@@ -30,10 +18,13 @@ namespace SavingPpm
             c.DrawLine(200,300,100,100, Colors.RED);
             c.FillCircle(50,50,50,Colors.GOLD);
             c.FillCircle(150,50,50,0xff181818);
-            c.FillRect(500,400,500,100,Colors.FINE_RED);
-            c.FillRect(500,600,500,100, Colors.GOLD);
-            c.FillRect(500,500,500,100,Colors.FINE_BLUE);
-            c.FillCircle(750,530,50,Colors.OLIVE);
+            c.FillCircle(0, 0, 50,Colors.TEAL);
+            c.FillRect(500, 400, 500, 100, Colors.FINE_RED);
+            c.FillRect(500, 600, 500, 100, Colors.GOLD);
+            c.FillRect(500, 500, 500, 100, Colors.FINE_BLUE);
+
+            c.FillCircle(1920, 1080, 60, Colors.OLIVE);
+            c.FillRect(1900, 10, 50,50, Colors.OLIVE);
             c.SaveAsPng("result.png");
 
 
